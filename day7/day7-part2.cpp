f8c631ef31bb00d0aaff59d427ca80039cf482aa
Andree L <andree.lr28@gmail.com>
2022-12-13 18:31:11 -0500

    day 7

---
+++ day7/day7-part2.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> sol;
+struct file{
+    string name;
+    int size;
+};
+
+struct folder {
+    string name;
+    int totalsize = 0;
+    vector<struct file*> files;
+    struct folder* parent;
+    vector<struct folder*> subfolders;
+    void sum(){
+        int total = 0;
+        for(auto e : files) total += e->size;
+        for(auto e : subfolders) total += e->totalsize;
+        totalsize = total;
+        if(parent != NULL){
+            parent->sum();
+        }
+    };
+    void print(){
+        for (auto e : subfolders)
+        {
+            cout<<e->name <<" "<<e->totalsize<<endl;
+            sol.push_back(e->totalsize);
+            if(e->subfolders.size() > 0) e->print();
+        }
+
+    };
+    int solve(){
+        int ans = 0;
+        for (auto e : subfolders)
+        {
+            if(e->totalsize <= 100000) ans += e->totalsize;
+            // cout<<e->name <<" "<<e->totalsize<<endl;
+            if(e->subfolders.size() > 0) ans += e->solve();
+        }
+        return ans;
+
+    }
+};
+
+
+int main(){
+
+    folder* src = new folder();
+    src->name = "/";
+    src->parent = NULL;
+
+    string input, command, dir;
+    int size;
+    string name;
+
+    folder* directory = src;
+
+    while(cin>>input){
+        if(input == "$"){
+            cin>>command;
+            if(command == "cd"){
+                cin>>dir;
+                if(dir == "/") directory = src;
+                else if (dir == ".."){
+                    directory = directory->parent;
+                }
+                else {
+                    for (auto e : directory->subfolders){
+                        if(e->name == dir) directory = e;
+                    }
+                }
+            }
+            
+        }
+        //read here
+        else {
+            if(input == "dir"){
+                cin>> dir;
+                folder* tmp = new folder();
+                tmp->name = dir;
+                tmp->parent = directory;
+                directory->subfolders.push_back(tmp);               
+                // cout<< "folder " +dir<<endl;
+            }
+            else {
+                cin >> name;
+
+                file* tmp  = new file();
+                tmp->name = name;
+                tmp->size = stoi(input);
+                directory->files.push_back(tmp);
+                directory->sum();
+                // directory->elements += stoi(input);
+                // cout<< file + " file "<<input<<endl;
+
+            }
+
+        }
+
+    }
+
+    
+    int storage = 70000000;
+    int need = 30000000;
+    storage -= src->totalsize;
+    
+    src->print();
+
+    sort(sol.begin(), sol.end());
+    cout<<"SOL: ";
+    for (auto e : sol){
+        if(storage + e >= need){
+            cout<<e<<endl;
+            break;
+        }
+    }
+    // cout<<src->solve()<<endl;
+
+
+
+
+    return 0;
+}
